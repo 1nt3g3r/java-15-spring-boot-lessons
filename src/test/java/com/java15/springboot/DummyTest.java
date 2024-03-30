83061a41ca93c955b95c576eea11dcea621734c4
Ivan Melnichuk <melnichuk.cadet@gmail.com>
2024-03-30 10:32:16 +0200

    Added dummy failed test

---
+++ src/test/java/com/java15/springboot/DummyTest.java
@@ -0,0 +1,11 @@
+package com.java15.springboot;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class DummyTest {
+    @Test
+    public void doSomething() {
+        Assertions.fail("Just dummy fail");
+    }
+}
